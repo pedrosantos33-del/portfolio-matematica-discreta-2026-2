704d4e6a727154b219d0f33684c769458d047d4f
pedrosantos33-del <pedro.santos33@iesb.edu.br>
2026-09-07 20:18:52 -0300

    Iniciando os trabalhos

--- P1/atividade_01_conjuntos/conjuntos.py
+++ P1/atividade_01_conjuntos/conjuntos.py
@@ -2,5 +2,8 @@ A = set(input("Digite os elementos do conjunto A: ").split())
 B = set(input("Digite os elementos do conjunto B: ").split())
 
 uniao = A | B
-
 print(uniao)
+
+interseccao = A & B
+print(interseccao)
+
