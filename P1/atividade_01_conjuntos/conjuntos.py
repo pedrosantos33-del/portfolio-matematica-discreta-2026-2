26eff97dbc7aad0593d967076dca3b2055ddc3a5
pedrosantos33-del <pedro.santos33@iesb.edu.br>
2026-09-09 13:48:21 -0300

    Aprendendo a complexidade de fazer o conjunto das partes

--- P1/atividade_01_conjuntos/conjuntos.py
+++ P1/atividade_01_conjuntos/conjuntos.py
@@ -1,4 +1,4 @@
-A = set(input("Digite os elementos do conjunto A: ").split())
+A = set(input("Digite os elementos do conjunto A: ").split()) 
 B = set(input("Digite os elementos do conjunto B: ").split())
 
 uniao = A | B
@@ -21,3 +21,22 @@ print("Cardinalidade de A:", cardinalidadeA)
 print("Cardinalidade de B:", cardinalidadeB)
 print("Cardinalidade de A ∪ B:", cardinalidade_uniao)
 print("Cardinalidade de A ∩ B:", cardinalidade_intersecçao)
+
+def gerar_partes(conjunto):
+    partes = {frozenset()}
+
+    for elemento in conjunto:
+        nova_partes = set()
+        for subset in partes:
+            nova_partes.add(subset | {elemento})
+        partes |= nova_partes
+
+    return partes
+
+
+partesA = gerar_partes(A)
+partesB = gerar_partes(B)
+
+print("Conjunto das partes de A:", partesA)
+print("Conjunto das partes de B:", partesB)
+        
